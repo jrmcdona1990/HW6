0687250920cb4a0c27b4f3af4ef4127e8fd2056a
John <john-r-mcdonald@uiowa.edu>
2013-11-11 22:35:06 -0600

    wireframe

--- MasterPage.master.vb
+++ MasterPage.master.vb
@@ -1,5 +1,9 @@
 ﻿
 Partial Class MasterPage
     Inherits System.Web.UI.MasterPage
+
+    Protected Sub Label1_Load(sender As Object, e As EventArgs) Handles Label1.Load
+        Label1.Text = "Today Is " & DateTime.Now.ToLongDateString
+    End Sub
 End Class
 
